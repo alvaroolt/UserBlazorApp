0b023934fac3eeed926774a2c95a6838beb4df3f
Alvaro L <alvarooleto@gmail.com>
2022-06-05 17:15:53 +0200

    save mesa

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace UserBlazorApp
             //});
             services.AddSingleton(new HttpClient
             {
-                BaseAddress = new Uri("http://192.168.1.185:44322")
+                BaseAddress = new Uri("https://localhost:44322")
             });
             services.AddSyncfusionBlazor(); // agrega el servicio de Syncfusion para Blazor
         }
